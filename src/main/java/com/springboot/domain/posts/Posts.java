af36399bbeeea89d7f89661e79e90248e858030c
aeiowoo <aeiowoo@gmail.com>
2022-02-22 20:11:25 +0900

    JUnit / JPA 실습 추가 (~100P)

---
+++ src/main/java/com/springboot/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id     // PK 필드
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // GenerationType.IDENTITY 으로 설정해야 auto_increment
+    private Long id;
+
+    @Column(length = 500, nullable = false)     // 테이블 컬럼 어노테이션, 해당 어노테이션 사용하지 않아도 클래스의 필드는 모두 컬럼이지만, 옵션 설정 시 사용
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder        // 생성자 상단에 해당 어노테이션 사용 시, 생성자에 포함된 필드만 빌더에 포함됨
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
